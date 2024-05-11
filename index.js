a8eec6ee91e8c7e299a393e5dae93bc14ef7f78a
DengelLionel <dengelriveracajincho@gmail.com>
2024-05-10 23:10:40 -0500

    aaa

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ app.get('/api/data', (req, res) => {
 });
 
 // Inicia el servidor
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
